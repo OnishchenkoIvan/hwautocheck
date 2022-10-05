3504ab8f884ecae9486fffc35db5844c819d09ba
Ivan Onishchenko <Lightsout07@mail.ru>
2022-10-05 18:45:43 +0300

    fifth exam #3

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -1,38 +1,56 @@
-import React, {FC} from 'react'
-import burgerIcon from './burger.svg'
-import s from './Header.module.css'
-import {useLocation} from 'react-router-dom'
-import {PATH} from '../Pages'
+import React, { FC } from "react";
+import burgerIcon from "./burger.svg";
+import s from "./Header.module.css";
+import { useLocation } from "react-router-dom";
+import { PATH } from "../Pages";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Menu,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 
 type PropsType = {
-    handleOpen: () => void
-}
+  handleOpen: () => void;
+};
 
-export const Header: FC<PropsType> = ({handleOpen}) => {
-    // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
-    const location = useLocation()
-    const currentPath = location.pathname
+export const Header: FC<PropsType> = ({ handleOpen }) => {
+  // hw5-menu изначально отсутствует, при нажатии на бургер - появляется, при повторном нажатии исчезает
+  const location = useLocation();
+  const currentPath = location.pathname;
 
-    const pageName =
-        currentPath === PATH.PRE_JUNIOR
-            ? 'Pre-junior'
-            : currentPath === PATH.JUNIOR
-                ? 'Junior'
-                : currentPath === PATH.JUNIOR_PLUS
-                    ? 'Junior Plus'
-                    : 'Error'
-    return (
-        <>
-            <div id={'hw5-header'} className={s.header}>
-                <img
-                    src={burgerIcon}
-                    id={'hw5-burger-menu'}
-                    className={s.burgerMenuIcon}
-                    onClick={handleOpen}
-                    alt={'open menu'}
-                />
-                <h1>{pageName}</h1>
-            </div>
-        </>
-    )
-}
+  const pageName =
+    currentPath === PATH.PRE_JUNIOR
+      ? "Pre-junior"
+      : currentPath === PATH.JUNIOR
+      ? "Junior"
+      : currentPath === PATH.JUNIOR_PLUS
+      ? "Junior Plus"
+      : "Error";
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar
+          style={{ justifyContent: "space-between", background: "#ffffff" }}
+        >
+          <img
+            style={{ paddingLeft: "73px" }}
+            src={burgerIcon}
+            id={"hw5-burger-menu"}
+            className={s.burgerMenuIcon}
+            onClick={handleOpen}
+            alt={"open menu"}
+          />
+          <Typography style={{ color: "#000000" }} variant="h6">
+            Hometask №5
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div id={"hw5-header"} className={s.header}>
+        <h1 style={{ margin: "0 auto" }}>{pageName}</h1>
+      </div>
+    </>
+  );
+};
