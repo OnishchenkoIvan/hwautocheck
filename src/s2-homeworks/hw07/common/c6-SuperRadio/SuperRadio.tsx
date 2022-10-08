5ae2ad1f086adc8fdf254c87636c3b293e2d4cb9
Ivan Onishchenko <Lightsout07@mail.ru>
2022-10-08 19:18:50 +0300

    seventh exam try#7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -48,7 +48,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ? options.map((o) => (
         <label key={name + "-" + o.id} className={s.label}>
           <input
-            id={id + "-input-" + value}
+            id={id + "-input-" + o.id}
             className={finalRadioClassName}
             type={"radio"}
             name={name}
@@ -64,7 +64,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
             {...spanProps}
             className={spanClassName}
           ></span>
-          {o.value}
+          {o.id}
         </label>
       ))
     : [];
