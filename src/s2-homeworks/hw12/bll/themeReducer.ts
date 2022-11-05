fdf66697132302ad7da8146424030e65fade4348
Ivan Onishchenko <Lightsout07@mail.ru>
2022-11-05 22:15:35 +0300

    hw12 logic done

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,33 @@
 const initState = {
-    themeId: 1,
-}
+  themeId: 1,
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export type StateType = {
+  themeId: number;
+};
 
-        default:
-            return state
+export type ActionType = {
+  type: "SET_THEME_ID";
+  id: number;
+};
+
+export const themeReducer = (
+  state: StateType = initState,
+  action: ReturnType<typeof changeThemeId>
+): StateType => {
+  // fix any
+  switch (action.type) {
+    // дописать
+    case "SET_THEME_ID": {
+      return { ...state, themeId: action.id };
     }
-}
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+    default:
+      return state;
+  }
+};
+
+export const changeThemeId = (id: number): ActionType => ({
+  type: "SET_THEME_ID",
+  id,
+}); // fix any
