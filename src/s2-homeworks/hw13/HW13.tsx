47971e96d231527dbd095638ab03085b12167b2f
Ivan Onishchenko <Lightsout07@mail.ru>
2022-11-06 22:12:07 +0300

    hw13 logic done #2

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -38,9 +38,7 @@ const HW13 = () => {
         setCode("Код 200!");
         setImage(success200);
         setInfo("");
-        setText(
-          "...всё ок)\n" + "код 200 - обычно означает что скорее всего всё ок)"
-        );
+        setText("...всё ок)");
         // дописать
       })
       .catch((e) => {
@@ -48,24 +46,19 @@ const HW13 = () => {
           setCode("Ошибка 400!");
           setImage(error400);
           setInfo("");
-          setText(
-            "Ты не отправил success в body вообще!\n" +
-              "ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!"
-          );
+          setText("Ты не отправил success в body вообще!");
         }
         if (e.response.status === 500) {
           setCode("Ошибка 500!");
           setImage(error500);
           setInfo("");
-          setText(
-            "эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)"
-          );
+          setText("эмитация ошибки на сервере");
         }
         if (e.response.status === 0) {
           setCode("Error!");
           setImage(errorUnknown);
           setInfo("");
-          setText("Network Error\n" + "AxiosError");
+          setText("Error");
         }
         // дописать
       });
